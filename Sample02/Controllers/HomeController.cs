c48f98419b894be163bfefdbdc81ba2c999a5bc3
chaotiandeng <969506643@qq.com>
2019-09-17 10:23:00 +0800

    新增Asp.Net Core示例代码Sample02

---
+++ Sample02/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace 配置文件读取.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly Book book;
+        public HomeController(IOptions<Book> option)
+        {
+            book = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(book);
+        }
+    }
+}
